05b4ac6f84e3be3238509fa734d87865a012fae0
Brian Nelson <bdnelson@gmail.com>
2022-10-10 09:13:35 -0500

    feat: Provide standard equipment types from API
    The /api/equipment_types endpoint returns a list of standard equipment
    types than can be used to drive the type selection logic in the
    frontend.

--- api/index.js
+++ api/index.js
@@ -7,14 +7,31 @@ const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get("/api/hello", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
-
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on ${PORT}`);
+});
+
+app.get("/api/hello", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+app.get("/api/info", (req, res) => {
+    res.json({ service: "Test API", version: "1.0.0" });
+});
+
+const EQUIPMENT_TYPES = [
+    "Heavy/Construction Equipment", 
+    "AG Equipment",
+    "Vehicle",
+    "Other"
+];
+
+app.get("/api/equipment_types", (req, res) => {
+    res.json(EQUIPMENT_TYPES);
+});
+
+// This needs to be the last route defined so that it does not
+// block the other defined routes since it is a wildcard match.
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
